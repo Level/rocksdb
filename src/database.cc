63af83b3c842767eca6f2be6e84ba9e72795095e
Nicola Del Gobbo <nicoladelgobbo@gmail.com>
2017-06-02 03:40:38 +0200

    Fixed deprecation warning on CompactRange

--- src/database.cc
+++ src/database.cc
@@ -81,7 +81,7 @@ uint64_t Database::ApproximateSizeFromDatabase (const rocksdb::Range* range) {
 
 void Database::CompactRangeFromDatabase (const rocksdb::Slice* start,
                                          const rocksdb::Slice* end) {
-  const rocksdb::CompactRangeOptions options;
+  rocksdb::CompactRangeOptions options;
   db->CompactRange(options, start, end);
 }
 
