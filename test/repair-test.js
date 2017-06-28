ceaea56347182b8b224025944ee33bb5e25f3948
Matteo Collina <hello@matteocollina.com>
2017-06-28 12:41:20 +0200

    Tests passing on Windows.

--- test/repair-test.js
+++ test/repair-test.js
@@ -27,10 +27,7 @@ test('test callback-less, 1-arg, repair() throws', function (t) {
 
 test('test repair non-existent directory returns error', function (t) {
   leveldown.repair('/1/2/3/4', function (err) {
-    if (process.platform !== 'win32')
-      t.ok(/^Error: NotFound:/i.test(err), 'error on callback')
-    else
-      t.ok(/IO error/i.test(err), 'error on callback')
+    t.ok(/^Error: NotFound:/i.test(err), 'error on callback')
     t.end()
   })
 })
