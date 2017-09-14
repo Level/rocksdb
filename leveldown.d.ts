4f2061659264e0c7ae7e1c8aeddb0d3f57f60595
Meirion Hughes <crakinshot@yahoo.com>
2017-09-14 23:55:47 +0100

    feat(typings): add initial typings

---
+++ leveldown.d.ts
@@ -0,0 +1,70 @@
+import * as Abstract from 'abstract-leveldown';
+
+declare namespace rocksdb {
+  export interface RocksDB<TKey, TValue>
+    extends Abstract.LevelDOWN<
+    TKey,
+    TValue,
+    RocksDBOptions,
+    RocksDBPutOptions,
+    RocksDBGetOptions,
+    RocksDBDeleteOptions,
+    RocksDBIteratorOptions<TKey, TValue>,
+    RocksDBBatchOptions> {
+    compactRange(start: TKey, end: TKey, cb: (err?: any) => void): void;
+    getProperty(property: string): string;
+    iterator(options?: RocksDBIteratorOptions<TKey, TValue>): RocksDBIterator<TKey>
+    destroy(location: string, cb: (err?: any) => void): void;
+    repair(location: string, cb: (err?: any) => void): void;
+  }
+
+  export interface RocksDBOptions {
+
+  }
+
+  export interface RocksDBPutOptions {
+    sync?: boolean
+  }
+
+  export interface RocksDBGetOptions {
+    fillCache?: boolean;
+    asBuffer?: boolean;
+  }
+
+  export interface RocksDBDeleteOptions {
+    sync?: boolean;
+  }
+
+  export interface RocksDBIteratorOptions<K, V> {
+    gt?: K;
+    gte?: K;
+    lt?: K;
+    lte?: K;
+    reverse?: boolean;
+    keys?: boolean;
+    values?: boolean;
+    limit?: number;
+    keyAsBuffer?: boolean;
+    valueAsBuffer?: boolean;
+  }
+
+  export interface RocksDBBatchOptions {
+    sync?: boolean;
+  }
+
+  export interface RocksDBIterator<TKey> extends Abstract.Iterator {
+    seek(key: TKey);
+    binding: any;
+    cache: any;
+    finished: any;
+    fastFuture: any;
+  }
+}
+declare function rocksdb<
+  TKey=any,
+  TValue=any
+  >(location: string)
+  : rocksdb.RocksDB<TKey, TValue>;
+
+
+export = rocksdb;
